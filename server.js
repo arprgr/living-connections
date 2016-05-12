de157028e81a0ea4050f4b7e3f1040b3c22d61e4
James Echmalian <ech@ech.net>
2016-05-12 16:00:02 -0400

    create hang-fire users

--- server.js
+++ server.js
@@ -1,7 +1,119 @@
 /* server.js */
 
 var express = require("express");
-var cool = require("cool-ascii-faces");
+var postgres = require("pg");
+var Promise = require("promise");
+
+var databaseUrl = process.env.DATABASE_URL || "postgres://postgres:postgres@localhost:5432/hangfire";
+console.log("Connecting to " + databaseUrl + "...");
+
+function replace(instr, token, replacementFunc) {
+	var outstr = "";
+	while (instr) {
+		var ix = instr.indexOf(token);
+		if (ix < 0) {
+			outstr += instr;
+			instr = null;
+		}
+		else {
+			outstr += instr.substring(0, ix);
+			outstr += replacementFunc();
+			instr = instr.substring(ix + 1);
+		}
+	}
+	return outstr;
+}
+
+function escapeArg(arg) {
+	if (arg == null) {
+		return "NULL";
+	}
+	else if (typeof arg == "string") {
+		return "'" + replace(arg, "'", "''") + "'";
+	}
+	else {
+		return arg.toString();
+	}
+}
+
+function bind(insql) {
+	var outsql = "";
+	var args = arguments;
+	var argIndex = 1;
+	return replace(insql, "?", function() {
+		return escapeArg(args[argIndex++]);
+	});
+}
+
+function query(sqlFunc, resultProcessor) {
+	return new Promise(function(resolve, reject) {
+		try {
+			var sql = sqlFunc();
+			console.log(sql);
+			postgres.connect(databaseUrl, function(err, client, done) {
+				if (err) {
+					reject(err);
+				}
+				else {
+					client.query(sql, function(err, result) {
+						done();
+						if (err) {
+							reject(err);
+						}
+						else if (!resultProcessor) {
+							resolve(result);
+						}
+						else if (typeof resultProcessor == "function") {
+							resolve(resultProcessor(result));
+						}
+						else {
+							resolve(resultProcessor);
+						}
+					});
+				}
+			});
+		}
+		catch (e) {
+			reject(e);
+		}
+	});
+	return promise;
+}
+
+function getUserByGoogleId(googleId) {
+	return query(function() {
+		return bind("SELECT * FROM users where google_id=?", googleId);
+	}, function(result) {
+		return result.rows && result.rows.length ? result.rows[0] : null;
+	});
+}
+
+function insertUser(googleId, name) {
+	return query(function() {
+		return bind("INSERT INTO users (google_id, name) VALUES (?, ?) RETURNING *", 
+			googleId, name);
+	}, function(result) {
+		return result.rows[0];
+	});
+}
+
+function lookupUser(req) {
+	return new Promise(function(resolve, reject) {
+    var name = req.query.name || "Anonymous";
+    var googleId = req.query.google_id;
+		if (!googleId) {
+			throw "required parameter missing";
+		}
+		getUserByGoogleId(googleId).then(function(user) {
+			if (user) {
+				resolve(user);
+			}
+			else {
+				resolve(insertUser(googleId, name));
+			}
+		}, reject);
+	});
+}
 
 var server = express();
 
@@ -10,33 +122,16 @@ server.set("port", process.env.PORT || 8989);
 // Static assets.
 server.use(express.static("www"));
 
-// Geo-search endpoint.
-server.get("/api", function (req, res) {
+// Get user info.
+server.get("/u", function (req, res) {
 
   console.log("request", { uri: "/api", query: req.query });
 
-  function ok(hits) {
-    res.json({ status: "ok", results: hits });
-  }
-
-  function error(errmsg) {
-    res.json({ status: "error", msg: errmsg });
-  }
-
-  var params = {
-    n: parseFloat(req.query.n),
-    lang: req.query.lang || "en"
-  };
-
-  if (isNaN(params.latitude)) {
-    return error("latitude must be a number");
-  }
-
-  ok({});
-});
-
-server.get("/cool", function(req, res) {
-	res.send(cool());
+	lookupUser(req).then(function(results) {
+    res.json({ status: "ok", results: results });
+	}, function(error) {
+    res.json({ status: "error", error: error });
+	});
 });
 
 server.listen(server.get("port"), function () {
