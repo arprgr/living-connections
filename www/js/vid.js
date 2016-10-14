feabdf35ac3f8fe58ca2d4f2e16bc6ce6dd63bac
James Echmalian <ech@ech.net>
2016-10-14 14:41:37 -0400

    use utils.trace

--- www/js/vid.js
+++ www/js/vid.js
@@ -1,6 +1,6 @@
 // vid.js
 
-define([ "jquery", ], function($) {
+define([ "jquery", "utils" ], function($, u) {
 
   function newController() {
     var startTime;
@@ -24,7 +24,7 @@ define([ "jquery", ], function($) {
     }
 
     function gotStream(stream) {
-      trace('Received local stream');
+      u.trace('Received local stream');
       localVideo.srcObject = stream;
       // Add localStream to global scope so it's accessible from the browser console
       window.localStream = localStream = stream;
@@ -32,7 +32,7 @@ define([ "jquery", ], function($) {
     }
 
     function start() {
-      trace('Requesting local stream');
+      u.trace('Requesting local stream');
       $("#startButton").attr("disabled", true);
       navigator.mediaDevices.getUserMedia({
         audio: false,
@@ -47,26 +47,26 @@ define([ "jquery", ], function($) {
     function call() {
       $("#callButton").attr("disabled", true);
       $("#hangupButton").attr("disabled", false);
-      trace('Starting call');
+      u.trace('Starting call');
       startTime = window.performance.now();
       var videoTracks = localStream.getVideoTracks();
       var audioTracks = localStream.getAudioTracks();
       if (videoTracks.length > 0) {
-        trace('Using video device: ' + videoTracks[0].label);
+        u.trace('Using video device: ' + videoTracks[0].label);
       }
       if (audioTracks.length > 0) {
-        trace('Using audio device: ' + audioTracks[0].label);
+        u.trace('Using audio device: ' + audioTracks[0].label);
       }
       var servers = null;
       // Add pc1 to global scope so it's accessible from the browser console
       window.pc1 = pc1 = new RTCPeerConnection(servers);
-      trace('Created local peer connection object pc1');
+      u.trace('Created local peer connection object pc1');
       pc1.onicecandidate = function(e) {
         onIceCandidate(pc1, e);
       };
       // Add pc2 to global scope so it's accessible from the browser console
       window.pc2 = pc2 = new RTCPeerConnection(servers);
-      trace('Created remote peer connection object pc2');
+      u.trace('Created remote peer connection object pc2');
       pc2.onicecandidate = function(e) {
         onIceCandidate(pc2, e);
       };
@@ -79,9 +79,9 @@ define([ "jquery", ], function($) {
       pc2.onaddstream = gotRemoteStream;
 
       pc1.addStream(localStream);
-      trace('Added local stream to pc1');
+      u.trace('Added local stream to pc1');
 
-      trace('pc1 createOffer start');
+      u.trace('pc1 createOffer start');
       pc1.createOffer(
         offerOptions
       ).then(
@@ -91,26 +91,26 @@ define([ "jquery", ], function($) {
     }
 
     function onCreateSessionDescriptionError(error) {
-      trace('Failed to create session description: ' + error.toString());
+      u.trace('Failed to create session description: ' + error.toString());
     }
 
     function onCreateOfferSuccess(desc) {
-      trace('Offer from pc1\n' + desc.sdp);
-      trace('pc1 setLocalDescription start');
+      u.trace('Offer from pc1\n' + desc.sdp);
+      u.trace('pc1 setLocalDescription start');
       pc1.setLocalDescription(desc).then(
         function() {
           onSetLocalSuccess(pc1);
         },
         onSetSessionDescriptionError
       );
-      trace('pc2 setRemoteDescription start');
+      u.trace('pc2 setRemoteDescription start');
       pc2.setRemoteDescription(desc).then(
         function() {
           onSetRemoteSuccess(pc2);
         },
         onSetSessionDescriptionError
       );
-      trace('pc2 createAnswer start');
+      u.trace('pc2 createAnswer start');
       // Since the 'remote' side has no media stream we need
       // to pass in the right constraints in order for it to
       // accept the incoming offer of audio and video.
@@ -121,33 +121,33 @@ define([ "jquery", ], function($) {
     }
 
     function onSetLocalSuccess(pc) {
-      trace(getName(pc) + ' setLocalDescription complete');
+      u.trace(getName(pc) + ' setLocalDescription complete');
     }
 
     function onSetRemoteSuccess(pc) {
-      trace(getName(pc) + ' setRemoteDescription complete');
+      u.trace(getName(pc) + ' setRemoteDescription complete');
     }
 
     function onSetSessionDescriptionError(error) {
-      trace('Failed to set session description: ' + error.toString());
+      u.trace('Failed to set session description: ' + error.toString());
     }
 
     function gotRemoteStream(e) {
       // Add remoteStream to global scope so it's accessible from the browser console
       window.remoteStream = remoteVideo.srcObject = e.stream;
-      trace('pc2 received remote stream');
+      u.trace('pc2 received remote stream');
     }
 
     function onCreateAnswerSuccess(desc) {
-      trace('Answer from pc2:\n' + desc.sdp);
-      trace('pc2 setLocalDescription start');
+      u.trace('Answer from pc2:\n' + desc.sdp);
+      u.trace('pc2 setLocalDescription start');
       pc2.setLocalDescription(desc).then(
         function() {
           onSetLocalSuccess(pc2);
         },
         onSetSessionDescriptionError
       );
-      trace('pc1 setRemoteDescription start');
+      u.trace('pc1 setRemoteDescription start');
       pc1.setRemoteDescription(desc).then(
         function() {
           onSetRemoteSuccess(pc1);
@@ -168,27 +168,27 @@ define([ "jquery", ], function($) {
             onAddIceCandidateError(pc, err);
           }
         );
-        trace(getName(pc) + ' ICE candidate: \n' + event.candidate.candidate);
+        u.trace(getName(pc) + ' ICE candidate: \n' + event.candidate.candidate);
       }
     }
 
     function onAddIceCandidateSuccess(pc) {
-      trace(getName(pc) + ' addIceCandidate success');
+      u.trace(getName(pc) + ' addIceCandidate success');
     }
 
     function onAddIceCandidateError(pc, error) {
-      trace(getName(pc) + ' failed to add ICE Candidate: ' + error.toString());
+      u.trace(getName(pc) + ' failed to add ICE Candidate: ' + error.toString());
     }
 
     function onIceStateChange(pc, event) {
       if (pc) {
-        trace(getName(pc) + ' ICE state: ' + pc.iceConnectionState);
+        u.trace(getName(pc) + ' ICE state: ' + pc.iceConnectionState);
         console.log('ICE state change event: ', event);
       }
     }
 
     function hangup() {
-      trace('Ending call');
+      u.trace('Ending call');
       pc1.close();
       pc2.close();
       pc1 = null;
@@ -197,23 +197,11 @@ define([ "jquery", ], function($) {
       $("#callButton").attr("disabled", false);
     }
 
-    function trace(text) {
-      if (text[text.length - 1] === '\n') {
-        text = text.substring(0, text.length - 1);
-      }
-      if (window.performance) {
-        var now = (window.performance.now() / 1000).toFixed(3);
-        console.log(now + ': ' + text);
-      } else {
-        console.log(text);
-      }
-    }
-
     function setLocalVideo(_localVideo) {
       localVideo = _localVideo;
 
       localVideo.addEventListener('loadedmetadata', function() {
-        trace('Local video videoWidth: ' + this.videoWidth +
+        u.trace('Local video videoWidth: ' + this.videoWidth +
           'px,  videoHeight: ' + this.videoHeight + 'px');
       });
     }
@@ -222,18 +210,18 @@ define([ "jquery", ], function($) {
       remoteVideo = _remoteVideo;
 
       remoteVideo.addEventListener('loadedmetadata', function() {
-        trace('Remote video videoWidth: ' + this.videoWidth +
+        u.trace('Remote video videoWidth: ' + this.videoWidth +
           'px,  videoHeight: ' + this.videoHeight + 'px');
       });
 
       remoteVideo.onresize = function() {
-        trace('Remote video size changed to ' +
+        u.trace('Remote video size changed to ' +
           remoteVideo.videoWidth + 'x' + remoteVideo.videoHeight);
         // We'll use the first onresize callback as an indication that video has started
         // playing out.
         if (startTime) {
           var elapsedTime = window.performance.now() - startTime;
-          trace('Setup time: ' + elapsedTime.toFixed(3) + 'ms');
+          u.trace('Setup time: ' + elapsedTime.toFixed(3) + 'ms');
           startTime = null;
         }
       };
