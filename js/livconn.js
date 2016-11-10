f04f2c8fcc0fc23e3f9e4f0f5797a5eaa3380684
James Echmalian <ech@ech.net>
2016-11-10 15:07:23 -0500

    spiff up the animation

--- js/livconn.js
+++ js/livconn.js
@@ -2,27 +2,43 @@
 
 define([ "jquery" ], function($) {
 
-  $("body")
-    .append($("<div>")
-      .addClass("startupScreen")
-      .append($("<div>").addClass("logo"))
-      .append($("<div>").css("height", 10))
-      .append($("<div>").addClass("dots").append($("<canvas>").attr("id", "theCanvas").css("height", 50)))
-    );
+  function render() {
+    var dots = $("<div>").addClass("dots");
 
-  function drawCircle(canvasId, color) {
+    for (var i = 0; i < 4; ++i) {
+      dots.append($("<canvas>").attr("id", "dot" + i).css("height", 50));
+    }
+
+    $("body")
+      .append($("<div>")
+        .attr("ng-controller", "StartupController")
+        .addClass("startupScreen")
+        .append($("<div>").addClass("logo"))
+        .append($("<div>").css("height", 10))
+        .append(dots));
+  }
+  render();
+
+  function clearCanvas(canvasId) {
+    var canvas = document.getElementById(canvasId);
+    var context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function drawCircle(canvasId, c1, c2, alpha) {
     var canvas = document.getElementById(canvasId);
     var context = canvas.getContext("2d");
     var centerX = canvas.width / 2;
     var centerY = canvas.height / 2;
     var radius = Math.min(centerX, centerY);
+    var gradient = context.createRadialGradient(75,50,5,90,60,100);
+    gradient.addColorStop(0, c1);
+    gradient.addColorStop(1, c2);
+    context.globalAlpha = alpha;
     context.beginPath();
     context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = color;
+    context.fillStyle = gradient;
     context.fill();
-    context.lineWidth = 5;
-    context.strokeStyle = '#003300';
-    context.stroke();
   }
 
   var done = false;
@@ -31,21 +47,34 @@ define([ "jquery" ], function($) {
     done = true;
   }
 
-  var interval;
-  var count = 0;
-
-  function animationStep() {
-    if (done) {
-      clearInterval(interval);
-      $("#theCanvas").remove();
-    }
-    else {
-      drawCircle("theCanvas", count++ % 2 == 0 ? "green" : "white");
+  function StartupController() {
+    setTimeout(signalTermination, 15000);
+    for (var i = 0; i < 4; ++i) {
+      setTimeout((function(ix) {
+        var startTime;
+        var interval;
+        var canvasId = "dot" + ix;
+        return function() {
+          startTime = new Date().getTime();
+          interval = setInterval(function() {
+            clearCanvas(canvasId);
+            if (done) {
+              clearInterval(interval);
+            }
+            else {
+              var frame = ((new Date().getTime() - startTime) % 2500) / 25;
+              var alpha = Math.max(0, (50 - frame) / 50);
+              drawCircle(canvasId, "white", "green", alpha);
+            }
+          }, 25);
+        };
+      })(i), (i + 1) * 750);
     }
   }
 
   return function() {
-    setTimeout(signalTermination, 5000);
-    interval = setInterval(animationStep, 500);
+    angular.module("Startup", [])
+      .controller("StartupController", [ StartupController ]);
+    angular.bootstrap(document, [ "Startup" ]);
   }
 });
