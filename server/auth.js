d7d5627246615a910c1a502cceb98a42c1617fd9
James Echmalian <ech@ech.net>
2016-12-26 15:11:49 -0500

    rework auth module AGAIN, this time for invitations

--- server/auth.js
+++ server/auth.js
@@ -19,6 +19,12 @@ const USER_WITH_THAT = {
   }]
 }
 
+const SUPERUSER = {
+  id: 0,
+  name: "Root",
+  level: 0
+}
+
 // Look up a session given its external ID, maybe from a cookie.
 function findSession(externalId) {
   return models.Session.findByExternalId(externalId, USER_WITH_THAT);
@@ -26,6 +32,7 @@ function findSession(externalId) {
 
 // Look up a session seed, given an email address.
 function findEmailSessionSeed(externalId) {
+  // TODO: If the session seed is no longer valid, ignore it.
   return models.EmailSessionSeed.findByExternalId(externalId);
 }
 
@@ -50,10 +57,13 @@ function createNewEmailProfile(email, userId) {
 }
 
 // Create a session.
-function createNewSession(userId) {
+function createNewSession(user) {
   return models.Session.create({
     externalId: random.id(),
-    userId: userId
+    userId: user.id
+  }).then(function(session) {
+    session.user = user;   // decorate the session with the user, just as findSession does.
+    return session;
   });
 }
 
@@ -73,92 +83,120 @@ function findOrCreateUserByEmail(email) {
   })
 }
 
+function Transaction(req, res, next) {
+  this.req = req;
+  this.res = res;
+  this.next = next;
+
+  var sessionCookie = req.cookies.s && req.cookies.s;
+  this.sessionCookie = sessionCookie;
+  this.sessionId = sessionCookie;
+
+  this.eseed = req.query && req.query.e;
+}
+
+function Transaction_lookupSession(self) {
+  if (self.sessionId) {
+    return findSession(self.sessionId).then(function(session) {
+      self.session = session;
+      return self;
+    });
+  }
+  else {
+    return Promise.resolve(self);
+  }
+}
+
+function Transaction_lookupEmailSessionSeed(self) {
+  if (self.eseed) {
+    return findEmailSessionSeed(self.eseed).then(function(emailSessionSeed) {
+      self.emailSessionSeed = emailSessionSeed;
+      return self;
+    });
+  }
+  else {
+    return Promise.resolve(self);
+  }
+}
+
 // Transmit session ID to client.
-function sendSessionCookie(res, sessionId) {
+function Transaction_sendSessionCookie(self) {
+  res = self.res;
+  sessionId = self.session.externalId;
   res.cookie("s", sessionId, {
     maxAge: 2147483647,
     path: "/",
   });
 }
 
-function Transaction(req, res, next) {
-  this.req = req;
-  this.res = res;
-  this.next = next;
+function Transaction_snapEmailSessionSeed(self) {
+}
+
+function Transaction_resolveSession(self) {
+  // If a user clicks on an invitation/ticket email:
+  if (self.emailSessionSeed) {
+    // If the user is already logged in...
+    if (self.session) {
+      // Log out.
+      models.Session.destroyByExternalId(self.sessionId);
+      self.session = null;
+    }
+    // Find the user with the given email address, or create one.
+    return findOrCreateUserByEmail(self.emailSessionSeed.email)
+    // Log in.
+    .then(createNewSession)
+    .then(function(session) {
+      self.session = session;
+      Transaction_sendSessionCookie(self);
+    })
+    .then(function() {
+      Transaction_snapEmailSessionSeed(self);
+      return self;
+    });
+  }
+  else {
+    return Promise.resolve(self);
+  }
+}
+
+// Got secret access key?
+function hasSecretAccessKey(req) {
+  return req.headers["x-access-key"] === CONFIG.adminKey;
+}
+
+// Does the request originate from the local host?
+function isLocalRequest(req) {
+  switch (req.headers["x-forwarded-for"] || req.connection.remoteAddress) {
+  case "127.0.0.1":
+  case "::1":
+    return true;
+  }
+  return false;
+}
+
+// Should we grant this request access to superuser functions?
+function Transaction_isSuperUser(self) {
+  return hasSecretAccessKey(self.req) || isLocalRequest(self.req);
 }
 
 Transaction.prototype = {
-  resolveBySessionId: function() {
-    var self = this;
-    var sessionId = self.req.cookies && self.req.cookies.s;
-    if (sessionId) {
-      return findSession(sessionId)
-      .then(function(session) {
-        if (session) {
-          self.req.session = session;
-          self.req.user = session.user;
-          return true;
-        }
-        return false;
-      })
-    }
-    else {
-      return Promise.resolve(false);
-    }
+  lookupSession: function() {
+    return Transaction_lookupSession(this);
   },
-  resolveByEmailSessionSeed: function() {
-    var self = this;
-    var eseed = self.req.query && self.req.query.e;
-    if (eseed) {
-      return findEmailSessionSeed(eseed)
-      .then(function(emailSessionSeed) {
-        if (emailSessionSeed) {
-          return findOrCreateUserByEmail(emailSessionSeed.email)
-          .then(function(user) {
-            self.req.user = user;
-            return createNewSession(user.id);
-          })
-          .then(function(session) {
-            sendSessionCookie(self.res, session.externalId);
-            self.req.session = session;
-            return true;
-          })
-        }
-        return false;
-      })
-    }
-    else {
-      return Promise.resolve(false);
-    }
+  lookupEmailSessionSeed: function() {
+    return Transaction_lookupEmailSessionSeed(this);
   },
-  resolveByInvitation: function() {
-    return Promise.resolve(false);
+  sendSessionCookie: function() {
+    return Transaction_sendSessionCookie(this);
   },
-  hasAdminKey: function() {
-    return this.req.headers["x-access-key"] === CONFIG.adminKey;
+  snapEmailSessionSeed: function() {
+    return Transaction_snapEmailSessionSeed(this);
   },
-  fromLocalHost: function() {
-    // Does the request originate from the local host?
-    switch (this.req.headers["x-forwarded-for"] || this.req.connection.remoteAddress) {
-    case "127.0.0.1":
-    case "::1":
-      return true;
-    }
+  resolveSession: function() {
+    return Transaction_resolveSession(this);
   },
-  becomeSuperUser: function() {
-    var self = this;
-    var success = false;
-    if (self.hasAdminKey() || self.fromLocalHost()) {
-      // Auto-login as admin.
-      // TODO: lock down this potential security hole.
-      self.req.user = {
-        id: 0,
-        name: "Root",
-        level: 0
-      }
-      success = true;
-    }
-    return Promise.resolve(success);
+  isSuperUser: function() {
+    return Transaction_isSuperUser(this);
   }
 }
 
@@ -167,21 +205,22 @@ Transaction.prototype = {
 // Based on request attributes, attach a user and session to the request object.
 //
 module.exports = function(req, res, next) {
-
-  var tx = new Transaction(req, res, next);
-
-  // TODO: handle the case of following a link while session already exists.
-  tx.resolveBySessionId(req.cookies.s, req, next)
-  .then(function(resolved) {
-    return resolved ? true : tx.resolveByEmailSessionSeed()
+  new Transaction(req, res, next).lookupSession()
+  .then(function(tx) {
+    return tx.lookupEmailSessionSeed();
   })
-  .then(function(resolved) {
-    return resolved ? true : tx.resolveByInvitation();
+  .then(function(tx) {
+    return tx.resolveSession();
   })
-  .then(function(resolved) {
-    return resolved ? true : tx.becomeSuperUser();
-  })
-  .then(function() {
+  .then(function(tx) {
+    if (tx.session) {
+      req.session = tx.session;
+      req.user = tx.session.user;
+    }
+    else if (tx.isSuperUser()) {
+      // TODO: lock down this potential security hole.
+      req.user = SUPERUSER;
+    }
     next();
   })
   .catch(next);
