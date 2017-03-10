7764cc00c4f8685eea43c73b96ad5493f2e7a0e2
arprgr <gregory.pillai@gmail.com>
2017-03-10 11:54:12 -0500

    changes for Reminders API

--- server/api/index.js
+++ server/api/index.js
@@ -3,14 +3,17 @@
 var router = require("express").Router();
 
 // All API functions require some sort of authentication.
-router.use(function(req, res, next) {
-  if (!req.isAdmin && !req.user) {
+
+router.use(function(req, res, next) {    
+  console.log('Info: Current User:' + req.user + ' Is Admin:' + req.isAdmin )  ;  
+ 
+ if (!req.isAdmin && !req.user) {
     next({ status: 401 });
   }
   else {
     next();
   }
-});
+}); 
 
 router.use("/connections", require("./connections_api"));
 router.use("/emailprofiles", require("./email_profiles_api"));
@@ -19,5 +22,6 @@ router.use("/messages", require("./messages_api"));
 router.use("/profile", require("./profile_api"));
 router.use("/users", require("./users_api"));
 router.use("/tickets", require("./tickets_api"));
+router.use("/reminders", require("./reminders_api"));
 
 module.exports = router;
