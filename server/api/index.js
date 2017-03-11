c026417383c26c70a2d245bb6f329c7967c1f150
James Echmalian <ech@ech.net>
2017-03-11 12:44:46 -0500

    Cosmetics

--- server/api/index.js
+++ server/api/index.js
@@ -4,16 +4,14 @@ var router = require("express").Router();
 
 // All API functions require some sort of authentication.
 
-router.use(function(req, res, next) {    
-  console.log('Info: Current User:' + req.user + ' Is Admin:' + req.isAdmin )  ;  
- 
- if (!req.isAdmin && !req.user) {
+router.use(function(req, res, next) {
+  if (!req.isAdmin && !req.user) {
     next({ status: 401 });
   }
   else {
     next();
   }
-}); 
+});
 
 router.use("/connections", require("./connections_api"));
 router.use("/emailprofiles", require("./email_profiles_api"));
--- server/conf.js
+++ server/conf.js
@@ -23,8 +23,8 @@ const DEFAULTS = {
     }
   },
   auth: {
-        grantAdminToLocalRequest: false,
-        enableExtHeaderAccess: false
+    grantAdminToLocalRequest: false,
+    enableExtHeaderAccess: false
   },
   clientServiceConfigurations: {
     apiService: {
@@ -75,9 +75,9 @@ const OVERRIDES = {
       }
     },
     auth: {
-        grantAdminToLocalRequest: true,
-        enableExtHeaderAccess: true
-  },  
+      grantAdminToLocalRequest: true,
+      enableExtHeaderAccess: true
+    },  
     clientServiceConfigurations: {
       sessionManager: {
         config: {
